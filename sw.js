e4d2d36251dfd2306fe46574b06237a96ba33381
Michael Jefferson <mtj8033@gmail.com>
2019-01-01 19:48:49 -0500

    updating worker with some routes

--- sw.js
+++ sw.js
@@ -2,6 +2,49 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox
 
 if (workbox) {
     console.log(`Yay! Workbox is loaded 🎉`);
+
+    workbox.core.setCacheNameDetails({
+        prefix: 'pwa-test',
+        suffix: 'v1',
+    });
+    workbox.routing.registerRoute(
+        new RegExp('.*\.js'),
+        workbox.strategies.networkFirst()
+    );
+    workbox.routing.registerRoute(
+        // Cache CSS files
+        /.*\.css/,
+        // Use cache but update in the background ASAP
+        workbox.strategies.staleWhileRevalidate({
+            // Use a custom cache name
+            cacheName: 'css-cache',
+        })
+    );
+
+    workbox.routing.registerRoute(
+        // Cache image files
+        /.*\.(?:png|jpg|jpeg|svg|gif)/,
+        // Use the cache if it's available
+        workbox.strategies.cacheFirst({
+            // Use a custom cache name
+            cacheName: 'image-cache',
+            plugins: [
+                new workbox.expiration.Plugin({
+                    // Cache only 20 images
+                    maxEntries: 20,
+                    // Cache for a maximum of a week
+                    maxAgeSeconds: 7 * 24 * 60 * 60,
+                })
+            ],
+        })
+    );
+
+
+
+    workbox.routing.registerRoute(
+        'https://cdn.jsdelivr.net/npm/vue@2.5.21/dist/vue.js',
+        workbox.strategies.staleWhileRevalidate(),
+    );
 } else {
     console.log(`Boo! Workbox didn't load 😬`);
 }
