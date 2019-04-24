38143e8fe4feb4b4c070882a29ff380c12147fcb
Jefferson, Mike <mtjefferson@paychex.com>
2019-04-23 22:28:30 -0400

    adding ignores

--- workbox/workbox-config.js
+++ workbox/workbox-config.js
@@ -1,8 +1,9 @@
 module.exports = {
-    "globDirectory": "dist/",
-    "globPatterns": [
+    globDirectory: ".",
+    globPatterns: [
         "**/*.{png,html,ico,js,css,webmanifest}"
     ],
-    "swDest": "sw.js",
-    "swSrc": "workbox/sw_shell.js"
+    swDest: "sw.js",
+    swSrc: "workbox/sw_shell.js",
+    globIgnores: ['node_modules/**/*', 'dist/**/*', 'workbox/**/*']
 };
